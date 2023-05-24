da06d8be5cd6179803ad9b3241742b4ba5c50f41
sopcoerik <sopcoerik@gmail.com>
2023-05-24 16:16:29 +0300

    Added local state and submit btn

--- test-project/library-management-redux/src/components/books/SearchBooks.js
+++ test-project/library-management-redux/src/components/books/SearchBooks.js
@@ -1,26 +1,38 @@
+import { useState } from "react";
 import { useTheme } from "../../hooks/useTheme";
+import Button from "../common/Button";
 
-function SearchBooks({ term, setTerm }) {
+function SearchBooks({ setTerm }) {
   const theme = useTheme();
 
+  const [localTerm, setLocalTerm] = useState("");
+
   const handleSearchTermChange = (e) => {
-    setTerm(e.target.value);
+    setLocalTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTerm(localTerm);
   };
 
   return (
     <div
-      className={`flex my-2 p-5 ${
+      className={`my-2 p-5 ${
         theme === "dark" ? "bg-black text-white" : "bg-slate-200"
       }  rounded`}
     >
-      <label>Search:</label>
-      <input
-        value={term}
-        onChange={handleSearchTermChange}
-        className={`w-full border border-gray-400 ml-2 ${
-          theme === "dark" && "bg-black text-white"
-        }`}
-      />
+      <form onSubmit={handleSubmit} className="flex w-full">
+        <label>Search:</label>
+        <input
+          value={localTerm}
+          onChange={handleSearchTermChange}
+          className={`w-full border border-gray-400 ml-2 ${
+            theme === "dark" && "bg-black text-white"
+          } px-3 outline-none`}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 }
