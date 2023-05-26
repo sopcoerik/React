f4f83ef010073fad3cd3624ef4b3a4fe3641d9d0
sopcoerik <sopcoerik@gmail.com>
2023-05-26 11:45:22 +0300

    Solved filter bug

--- test-project/library-management-redux/src/store/apis/booksApi.js
+++ test-project/library-management-redux/src/store/apis/booksApi.js
@@ -19,7 +19,9 @@ const booksApi = createApi({
           order = "asc",
           page = 1,
         } = {}) =>
-          `/books?&search=${term}&categoryId=${catId}&sortBy=${sortBy}&order=${order}&page=${page}&limit=5`,
+          `/books?&search=${term}${
+            catId ? "&categoryId=" + catId : ""
+          }&sortBy=${sortBy}&order=${order}&page=${page}&limit=5`,
       }),
 
       addBooks: builder.mutation({
